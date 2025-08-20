d555f8d95a1f28590314fd0e580a6c6551cb310c
Tapu45 <rameswarpanda891@gmail.com>
2025-08-21 02:47:31 +0530

    jj

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -27,6 +27,7 @@ const eslintConfig = [
       "@typescript-eslint/no-this-alias": "off",
       "@typescript-eslint/no-empty-object-type": "off",
       "@typescript-eslint/no-unsafe-function-type": "off",
+         "@typescript-eslint/no-require-imports": "off",
     },
   },
 ];
