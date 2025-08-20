478b27fa8e300fce06c969c8b3c32f1677d4dc26
Tapu45 <rameswarpanda891@gmail.com>
2025-08-21 03:02:43 +0530

    jhbjjjjkk

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -31,6 +31,9 @@ const eslintConfig = [
       "@typescript-eslint/no-empty-object-type": "off",
       "@typescript-eslint/no-unsafe-function-type": "off",
       "@typescript-eslint/no-require-imports": "off",
+      "@typescript-eslint/no-unnecessary-type-constraint": "off",
+      "@typescript-eslint/no-wrapper-object-types": "off",
+      "@next/next/no-img-element": "warn" // Change to warning instead of error
     },
   },
 ];
